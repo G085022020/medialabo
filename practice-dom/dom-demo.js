fb7a1348c8b2777acdb48dad226a6cc248c84f21
G085022020 <208502@st.takushoku-u.ac.jp>
2022-06-30 12:46:04 +0900

    demo

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -10,3 +10,25 @@ ul.insertAdjacentElement('beforeend', li); //liを追加
 li = document.createElement('li');
 li.textContent = 'ヨーロッパ大陸'; 
 ul.insertAdjacentElement('beforeend', li);
+
+
+let h2sevenOcean = document.querySelector('h2#sevenOcean');
+
+let oceans = [
+    '北極海',
+    '北大西洋',
+    '南大西洋',
+    'インド洋',
+    '北太平洋',
+    '南太平洋',
+    '南極海'
+  ]
+
+ul = document.createElement('ul');
+ul.insertAdjacentElement('beforeend', ul);
+
+for (let o of oceans){
+    li = document.createElement('li');
+    li.textContent = o;
+    ul.insertAdjacentElement('beforeend',li);
+}
