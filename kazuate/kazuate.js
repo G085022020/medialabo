51765612b798312b8eb1e0ce3fdaf721351737da
G085022020 <208502@st.takushoku-u.ac.jp>
2022-07-07 15:25:01 +0900

    new

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -16,25 +16,23 @@ hantei();
 function hantei() {
     let kaisu = kaisu + 1;
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
-    console.log(kaisu + "回目の予想" + yoso);
-    if (kotae === yoso && kaisu < 4){
-        console.log("正解です。おめでとう！");
-    }
+
+     if (kotae === yoso && kaisu < 4){
+        result =("正解です。おめでとう！");
+     }
     else if (kotae !== yoso && kaisu < 3 && kotae > yoso){
-        console.log("まちがい。答えはもっと大きいですよ");
+       result =("まちがい。答えはもっと大きいですよ");
     }
-    else if (kotae !== yoso && kaisu < 3 && kotae < yoso){
-        console.log("まちがい。答えはもっと小さいですよ");
-    }
-    else if (kotae !== yoso && kaisu === 3 ){
-        console.log("まちがい。残念でした答えは "+ kotae +" です．");
+     else if (kotae !== yoso && kaisu < 3 && kotae < yoso){
+        result =("まちがい。答えはもっと小さいですよ");
+     }
+     else if (kotae !== yoso && kaisu === 3 ){
+        result = ("まちがい。残念でした答えは "+ kotae +" です．");
     }
     else {
-        console.log("答えは "+ kotae +" でした．すでにゲームは終わっています")
+        result = ("答えは "+ kotae +" でした．すでにゲームは終わっています")
     }
-    // 課題3-1：ここの判定処理を作成する．。
-    //        ページに表示する方法はまだ習っていないので
-    //        判定結果はコンソールに出力すること
+
     let s = document.querySelector('span#kaisu');
     s.textContent = kaisu;
     let p = document.querySelector('span#yoso');
@@ -42,4 +40,5 @@ function hantei() {
     let q = document.querySelector('p#result');
     q.textContent = result;
 }
+
 
