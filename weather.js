3a8adc35d6c9158d7849c609235bf8b16c38fdff
G085022020 <208502@st.takushoku-u.ac.jp>
2022-07-27 19:33:48 +0900

    new

---
+++ weather.js
@@ -0,0 +1,103 @@
+let data = {
+  "coord": {
+    "lon": 116.3972,
+    "lat": 39.9075
+  },
+  "weather": [
+    {
+      "id": 803,
+      "main": "Clouds",
+      "description": "曇りがち",
+      "icon": "04d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 9.94,
+    "feels_like": 8.65,
+    "temp_min": 9.94,
+    "temp_max": 9.94,
+    "pressure": 1022,
+    "humidity": 14,
+    "sea_level": 1022,
+    "grnd_level": 1016
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 2.65,
+    "deg": 197,
+    "gust": 4.84
+  },
+  "clouds": {
+    "all": 53
+  },
+  "dt": 1646542386,
+  "sys": {
+    "type": 1,
+    "id": 9609,
+    "country": "CN",
+    "sunrise": 1646520066,
+    "sunset": 1646561447
+  },
+  "timezone": 28800,
+  "id": 1816670,
+  "name": "北京市",
+  "cod": 200
+};
+
+////////// 課題3-2 ここからプログラムを書こう
+
+let weather = {
+  apiKey: "cc76be87af46d7115c7e6ff71e40f61a",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Denver");
